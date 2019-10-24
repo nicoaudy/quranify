27ed9deeaed354a43ff9c14a4dd73c619013b668
Nico Audy <nicoaudy1994@gmail.com>
2019-10-24 17:08:56 +0700

    add stack row menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -58,6 +58,139 @@ class HomeScreen extends StatelessWidget {
                         color: Color(getColorHexFromStr('#FEE16D'))
                             .withOpacity(0.5))),
               ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 50.0),
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text(
+                      "Title",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text(
+                      "Become your subtitle",
+                      style: TextStyle(
+                        fontSize: 23.0,
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25.0),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search,
+                              color: Color(getColorHexFromStr('#FEDF62')),
+                              size: 30.0),
+                          contentPadding:
+                              EdgeInsets.only(left: 15.0, top: 15.0),
+                          hintText: 'Search',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontFamily: 'Quicksand',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Stack(
+                    children: <Widget>[
+                      SizedBox(height: 10.0),
+                      Material(
+                        elevation: 1.0,
+                        child: Container(height: 75.0, color: Colors.white),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Container(
+                            height: 75.0,
+                            width: 50.0,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  height: 50.0,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/quran.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 5.0),
+                                Text(
+                                  'Quran',
+                                  style: TextStyle(fontFamily: 'Quicksand'),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 75.0,
+                            width: 50.0,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  height: 50.0,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/hadits.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 7.0),
+                                Text(
+                                  'Hadits',
+                                  style: TextStyle(fontFamily: 'Quicksand'),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 75.0,
+                            width: 50.0,
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  height: 50.0,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/asmaul.png'),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 7.0),
+                                Text(
+                                  'Husna',
+                                  style: TextStyle(fontFamily: 'Quicksand'),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              )
             ],
           )
         ],
