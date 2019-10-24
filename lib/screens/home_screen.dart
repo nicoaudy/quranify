a0ee26bfd48b26e9654a29a339c94413b68f04b1
Nico Audy <nicoaudy1994@gmail.com>
2019-10-24 15:45:14 +0700

    stack container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  int getColorHexFromStr(String colorStr) {
+    colorStr = "FF" + colorStr;
+    colorStr = colorStr.replaceAll("#", "");
+    int val = 0;
+    int len = colorStr.length;
+    for (int i = 0; i < len; i++) {
+      int hexDigit = colorStr.codeUnitAt(i);
+      if (hexDigit >= 48 && hexDigit <= 57) {
+        val += (hexDigit - 48) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 65 && hexDigit <= 70) {
+        // A..F
+        val += (hexDigit - 55) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 97 && hexDigit <= 102) {
+        // a..f
+        val += (hexDigit - 87) * (1 << (4 * (len - 1 - i)));
+      } else {
+        throw new FormatException("An error occurred when converting a color");
+      }
+    }
+    return val;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 250.0,
+                width: double.infinity,
+                color: Color(getColorHexFromStr('#FDD148')),
+              ),
+              Positioned(
+                bottom: 50.0,
+                right: 100.0,
+                child: Container(
+                  height: 400.0,
+                  width: 400.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(200.0),
+                      color: Color(getColorHexFromStr('#FEE16D'))
+                          .withOpacity(0.4)),
+                ),
+              ),
+              Positioned(
+                bottom: 100.0,
+                left: 150.0,
+                child: Container(
+                    height: 300.0,
+                    width: 300.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(150.0),
+                        color: Color(getColorHexFromStr('#FEE16D'))
+                            .withOpacity(0.5))),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
