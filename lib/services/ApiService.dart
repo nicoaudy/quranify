234ad4019f37c36b39ec96fcaab6f9639bc9f3f6
Nico Audy <nicoaudy1994@gmail.com>
2019-10-26 13:03:45 +0700

    make api service and doa harian provider

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:hijra/models/asmaul_husna.dart';
 import 'package:hijra/models/detail_surah.dart';
+import 'package:hijra/models/doa_harian.dart';
 import 'package:hijra/models/surah_info.dart';
 
 class ApiService {
@@ -17,6 +18,13 @@ class ApiService {
     var res = json.decode(response);
     Iterable data = res['data'];
     return data.map((model) => AsmaulHusna.fromJson(model)).toList();
+  }
+
+  Future<List<DoaHarian>> loadDoaHarian() async {
+    String response = await rootBundle.loadString('static/doa-harian.json');
+    var res = json.decode(response);
+    Iterable data = res['data'];
+    return data.map((model) => DoaHarian.fromJson(model)).toList();
   }
 
   Future<DetailSurah> loadSurah(index) async {
