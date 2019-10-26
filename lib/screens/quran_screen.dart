e1f19dc41271d49c5a98a6a8d9f8e424bb30a805
Nico Audy <nicoaudy1994@gmail.com>
2019-10-26 11:46:48 +0700

    fix

--- lib/screens/quran_screen.dart
+++ lib/screens/quran_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hijra/components/card_info.dart';
 import 'package:hijra/screens/detail_surah_screen.dart';
-import 'package:hijra/screens/setting_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:hijra/providers/surah_info_provider.dart';
 
@@ -11,14 +10,10 @@ class QuranScreen extends StatelessWidget {
     final surahInfo = Provider.of<SurahInfoProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Quranify"),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.more_vert),
-              color: Theme.of(context).primaryColorLight,
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SettingScreen())))
-        ],
+        title: Text(
+          "AL-QURAN",
+          style: TextStyle(letterSpacing: 2.0, fontWeight: FontWeight.w700),
+        ),
       ),
       body: ListView.builder(
         itemCount: surahInfo.data.length,
